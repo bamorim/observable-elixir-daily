875a8ad0662d359155c073b7c69d0f291144e30e
Bernardo Amorim <contato@bamorim.com>
2022-06-05 23:26:49 +0100

    chore: setup seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+if Daily.Repo.aggregate(Daily.Tasks.Task, :count) == 0 do
+  Daily.Tasks.create_task(%{description: "Eat"})
+  Daily.Tasks.create_task(%{description: "Sleep"})
+  Daily.Tasks.create_task(%{description: "Prepare ElixirConf EU 2022 Slides"})
+  Daily.Tasks.create_task(%{description: "Repeat"})
+end
